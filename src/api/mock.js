9bfe7493d874f2a26b52f23cb5d343a37e5ce9af
Aspect13 <ad13box@gmail.com>
2023-11-01 18:09:16 +0400

    feature: adding mock api

---
+++ src/api/mock.js
@@ -0,0 +1,41 @@
+import { alitaApi } from "./alitaApi.js";
+
+
+const apiSlicePath = '/api_mock'
+const TAG_TYPE_AUTHOR = 'Author'
+
+export const mockApi = alitaApi.enhanceEndpoints({
+    addTagTypes: [TAG_TYPE_AUTHOR]
+}).injectEndpoints({
+// export const promptApi = alitaApi.injectEndpoints({
+    endpoints: build => ({
+        trendingAuthorsList: build.query({
+            query: (projectId) => ({
+                url: apiSlicePath + '/trending_authors/' + projectId,
+            }),
+            providesTags: (result, error) => {
+                if (error) {
+                    return []
+                }
+                return result?.map(i => ({type: TAG_TYPE_AUTHOR, id: i.id}))
+            }
+        }),
+        trendingAuthorsDetails: build.query({
+            query: ({projectId, userId}) => ({
+                url: apiSlicePath + '/trending_authors/' + projectId + '/' + userId,
+            }),
+            providesTags: (result, error) => {
+                if (error) {
+                    return []
+                }
+                return [{type: TAG_TYPE_AUTHOR, id: result.id}]
+            }
+        }),
+    })
+})
+
+export const {
+    useTrendingAuthorsListQuery,
+    useTrendingAuthorsDetailsQuery,
+} = mockApi
+
